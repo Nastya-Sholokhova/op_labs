cf88eb8cf0473e5b53a75b1531d3af89fddb0fa3
Alexey <plaxinrus@yandex.ru>
2021-04-08 12:56:40 +0500

    a

--- lab3a.c
+++ lab3a.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <math.h>
+int koren(float a)
+{
+    float b;
+    if (a > 0)
+        b = pow(a, (float)1 / 3);
+    else
+        b = -pow(a, (float)1 / 3);
+    return b;
+}
 
-int main() {
-  float x = -2, f, h;
-  int i, n;
-  printf("Введите шаг h ");
-  scanf("%f", &h);
-  printf("x       f(x) \n");
-  printf("---------------------------— \n");
-  n = 3 / h;
-  for (i = 1; i <= n; i++)
-  {
-      if ((-2 <= x) && (x <= 0))
-        f = x * x * sin(pow(x, 1/3) - 3);
-      else
-        f = sqrt(x) * cos(2 * x);
-      printf("x=%f f(x)= %f\n", x,f);
-      x += -2 + i * h;
-  }
+int main()
+{
+    float x = -2, f, h, n;
+    int i;
+    printf("Введите шаг h ");
+    scanf("%f", &h);
+    printf("x  f(x) \n");
+    printf("---------------------------— \n");
+    n = (3 / h) + 1;
+    for (i = 1; i <= n; i++)
+    {
+        if ((-2 <= x) && (x <= 0))
+            f = x * x * sin(koren(x) - 3);
+        else
+            f = sqrt(x) * cos(2 * x);
+        printf("%f   %f\n",x,f);
+        x = -2 + i * h;
+    }
 }
--- lab3b.c
+++ lab3b.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <math.h>
+int koren(float a)
+{
+    float b;
+    if (a > 0)
+        b = pow(a, (float)1 / 3);
+    else
+        b = -pow(a, (float)1 / 3);
+    return b;
+}
 
 int main()
 {
-  float x = -2, f, h;
+  float x = -2, f, h, n;
+  int i = 1;
   printf("Введите шаг h ");
-  scanf("%f\n", &h);
+  scanf("%f", &h);
   printf ("x f(x) \n");
   printf ("-----------------------\n");
+  n = (3 / h) + 1;
   do
     {
       if ((-2 <= x) && (x <= 0))
-        f = x * x * sin(pow(x, 1/3) - 3);
+        f = x * x * sin(koren(x) - 3);
       else
           f = sqrt(x) * cos(2 * x);
       printf("%f %f \n",x,f);
-      x += h;
+      x = -2 + i * h;
+      i += 1;
     }
-  while (x < (x + h));
+  while (i <= n);
 
 }
