b326325afc6f783952dcdd4ca8ff2350d0fe5c53
FatkhutdinovVD <fatkhutdinov@edu.surgu.ru>
2021-05-20 16:59:34 +0500

    aa

--- lab9.c
+++ lab9.c
@@ -3,25 +3,30 @@
 
 int main()
 {
-    int len, inew[256], j = 0;
+    int i, len, inew[256], j = 0, k = 0, t = 0;
     char str[256], new[256];
     scanf("%255[^\n]", &str);
     char delim[] = " , ";
     char *p = strtok(str, delim);
     len = strlen(p);
-    for (int i = 1; i < len; i++)
+
+    for (i = 0; i < 256; i++)
+        inew[i] = 0;
+
+    for (i = 0; i < len; i++)
     {
         if (p[0] == p[i])
         {
-          inew[j] = i;
-          j++;
+            inew[j] = i;
+            j++;
         }
     }
-    printf(*new);
+
     while (p = strtok(NULL, delim))
     {
         len = strlen(p);
-        for (int i = 1; i < len; i++)
+        new[0] = p[0];
+        for (i = 1; i < len; i++)
         {
             if (p[0] == p[i])
             {
@@ -30,10 +35,33 @@ int main()
             }
         }
     }
-    for (i = 0; i < 256; i++)
+
+    j = 0;
+
+    for (i = 0; i < strlen(str); i++)
+        if (str[i] == ' ')
+            k += 1;
+    for (i = 0; i < (k + 1); i++)
     {
-        printf("%d ", inew[i]);
+        do
+        {
+            if (t != inew[j])
+                new[t] = str[t];
+            t++;
+            j++;
+        } while (str[t] != ' ');
+        j = 0;
     }
+
+    printf("Chisla povtor: ");
+
+    for (i = 0; i < 256; i++)
+        if (inew[i] != 0)
+            printf("%d ", inew[i]);
+    printf("\n");
+    for (i = 0; i < strlen(new); i++)
+        printf("%s ", new);
+    printf(" ");
     printf("\n");
     return 0;
 }
