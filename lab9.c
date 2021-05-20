1ed5af4ec22c5ab56a05ebfd43fc1cdf7600427a
user <user@ws1013402139.pm>
2021-05-20 14:51:00 +0500

    aa

--- lab9.c
+++ lab9.c
@@ -3,34 +3,36 @@
 
 int main()
 {
-    int len;
-    ;
-    char str[256], *new[256];
+    int len, inew[256], j = 0;
+    char str[256], new[256];
     scanf("%255[^\n]", &str);
     char delim[] = " , ";
     char *p = strtok(str, delim);
     len = strlen(p);
-    *new[0] = p[0];
     for (int i = 1; i < len; i++)
     {
-        if (p[i] != p[0])
+        if (p[0] == p[i])
         {
-            new[i] = strcat(*new , p);
+          inew[j] = i;
+          j++;
         }
     }
     printf(*new);
     while (p = strtok(NULL, delim))
     {
         len = strlen(p);
-        *new[0] = p[0];
         for (int i = 1; i < len; i++)
         {
-            if (p[i] != p[0])
+            if (p[0] == p[i])
             {
-                new[i] = strcat(*new , p);
+                inew[j] = i;
+                j++;
             }
         }
-        printf(" %s", *new);
+    }
+    for (i = 0; i < 256; i++)
+    {
+        printf("%d ", inew[i]);
     }
     printf("\n");
     return 0;
